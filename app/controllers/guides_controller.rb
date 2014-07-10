2dbffcaabae4dc2e61af7e8639a18bd806b50ecc
Prelang Builder <builder@prelang.com>
2014-07-10 00:17:36 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,4 +1,8 @@
 class GuidesController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_guide, only: [:show, :edit, :update, :destroy]
 
   # GET /guides
