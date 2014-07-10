bf71c95522e508d7737e45784a648ab5161d9427
Prelang Builder <builder@prelang.com>
2014-07-10 00:17:31 +0000

    Defined validation for attribute 'website' on model 'School'

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,4 +2,5 @@ class School < ActiveRecord::Base
   has_many :guides
   validates_formatting_of :email, using: :email
   validates_formatting_of :phone, using: :us_phone
+  validates_formatting_of :website, using: :url
 end
