11a125c1c4f8a45338976a71f5581592ca658eaa
Prelang Builder <builder@prelang.com>
2014-07-10 00:17:30 +0000

    Defined validation for attribute 'picture_url' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,5 @@ class User < ActiveRecord::Base
 
   validates_formatting_of :email, using: :email
   validates_formatting_of :phone, using: :us_phone
+  validates_formatting_of :picture_url, using: :url
 end
